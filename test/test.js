ef4fb264f9618003a590b2a5b186824a0870c32c
Olatunbosun Adeoye <adex001@gmail.com>
2018-05-20 12:02:08 +0100

    script running locally

---
+++ test/test.js
@@ -0,0 +1,9 @@
+const chai = require('chai');
+const assert = chai.assert;
+const app = require('../app');
+
+describe('Gets the homepage', function(req, res){
+    it('should return the message', function(){
+        
+    })
+});
